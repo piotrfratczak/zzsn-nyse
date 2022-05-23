6fd3af7fdbec87ee3033fbbeaa25c56304c87213
Piotr Frątczak <piotrfratczak99@gmail.com>
2022-05-23 19:28:38 +0200

    Update GRU network

--- models/GRUNet.py
+++ models/GRUNet.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -6,14 +7,19 @@ class GRUNet(nn.Module):
     Gated Recurrent Unit.
     """
 
-    def __init__(self, d_in, h, d_out, gru_layers, preds_len):
-        super().__init__()
-        self.preds_len = preds_len
-        # self.h0 = torch.randn(gru_layers, batch_size, h)
-        self.gru = nn.GRU(d_in, h, batch_first=True, num_layers=gru_layers)
-        self.linear = nn.Linear(h, d_out)
+    def __init__(self, input_dim, hidden_dim, output_dim, gru_layers, dropout=0.2):
+        super(GRUNet, self).__init__()
+        self.preds_len = output_dim
+        self.gru_layers = gru_layers
+        self.hidden_dim = hidden_dim
+
+        self.gru = nn.GRU(input_dim, hidden_dim, num_layers=gru_layers, batch_first=True)
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
-        output, hn = self.gru(x)
-        output = self.linear(output[:, -self.preds_len:, :])
+        h0 = torch.zeros(self.gru_layers, x.size(0), self.hidden_dim)
+        output, hn = self.gru(x, h0.detach())
+        output = self.dropout(output[:, -self.preds_len:, :])
+        output = self.fc(output)
         return output
