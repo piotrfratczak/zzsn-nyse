3c66ed40c7f473a9d9352796825866c9d88165a4
Piotr Frątczak <piotrfratczak99@gmail.com>
2022-05-24 07:10:14 +0200

    Calculate log returns in data_analysis.py

--- utils/data_analysis.py
+++ utils/data_analysis.py
@@ -1,53 +1,77 @@
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 from load_data import *
+from utils.preprocessor import calc_log_returns
 
-df = load_file('nyse/prices-split-adjusted.csv')
 
-print(df.shape)
-print(df.head())
-print(df.tail())
+def analyze_data():
+    df = load_file('nyse/prices-split-adjusted.csv')
 
-print(df.describe())
+    print(df.shape)
+    print(df.head())
+    print(df.tail())
+    print('--------')
+    print(df.describe())
+    print('--------')
+    print(df.info())
+    print('--------')
+    print(f'Missing values: {df.isnull().sum().sum()}')
 
-plt.figure(figsize=(10, 10))
-sns.heatmap(df.corr(), annot=True, fmt='.1g', vmin=-1, vmax=1, center=0, linewidth=3, linecolor='black', square=True)
-plt.show()
+    symbols = list(set(df.symbol))
+    print(symbols[:10])
 
-print(df.info())
+    log_returns = df.copy()
+    price_columns = ['open', 'close', 'high', 'low']
+    numeric_columns = ['open', 'close', 'high', 'low', 'volume']
+    log_returns[price_columns] = calc_log_returns(log_returns[price_columns])
+    log_returns[numeric_columns] = StandardScaler().fit_transform(log_returns[numeric_columns])
+    plt.figure(figsize=(10, 10))
+    sns.heatmap(log_returns.corr(),
+                annot=True, fmt='.1g', vmin=-1, vmax=1, center=0, linewidth=3, linecolor='black', square=True)
+    plt.show()
 
-print(f'Missing values: {df.isnull().sum().sum()}')
+    plot_symbol = 'AAPL'
+    plot_df = df[df['symbol'] == plot_symbol]
+    plt.figure(figsize=(22, 18))
+    x = plot_df.date
+    plt.subplot(2, 1, 1)
+    plt.plot(x, plot_df.open.values, color="red", label="Opening Price")
+    plt.plot(x, plot_df.close.values, color="blue", label="Closing Price")
+    plt.title(f"Stock Prices {plot_symbol} 2010-2016", fontsize=18)
+    plt.ylabel("Stock Prices in USD", fontsize=18)
+    plt.legend(loc="best")
+    plt.grid(which="major", axis="both")
+    plt.xticks(x[::50],  rotation='vertical')
 
-symbols = list(set(df.symbol))
-print(symbols[:10])
+    plt.subplot(2, 1, 2)
+    plt.plot(x, plot_df.volume.values, color="green", label="Stock Volume Available")
+    plt.title(f"Stock Volume of {plot_symbol} 2010-2016", fontsize=18)
+    plt.xlabel("Date", fontsize=18)
+    plt.ylabel("Volume", fontsize=18)
+    plt.legend(loc="best")
+    plt.grid(which="major", axis="both")
+    plt.xticks(x[::50],  rotation='vertical')
+    plt.show()
 
-plot_symbol = 'AAPL'
-plot_df = df[df['symbol'] == plot_symbol]
-plt.figure(figsize=(20, 12))
-x = np.arange(0, plot_df.shape[0], 1)
-plt.subplot(2, 1, 1)
-plt.plot(x, plot_df.open.values, color="red", label="Opening Price")
-plt.plot(x, plot_df.close.values, color="blue", label="Closing Price")
-plt.title(f"Stock Prices {plot_symbol} 2010-2016", fontsize=18)
-plt.xlabel("Days", fontsize=18)
-plt.ylabel("Stock Prices in USD", fontsize=18)
-plt.legend(loc="best")
-plt.grid(which="major", axis="both")
+    plt.figure(figsize=(22, 12))
+    plot_returns = log_returns[log_returns['symbol'] == plot_symbol]
+    plt.plot(plot_returns.date, plot_returns.close, color="violet", label="Logarithmic Returns")
+    plt.title(f"Stock Logarighmic Returns of {plot_symbol} 2010-2016", fontsize=18)
+    plt.xlabel("Date", fontsize=18)
+    plt.ylabel("Standardized Log Returns", fontsize=18)
+    plt.legend(loc="best")
+    plt.grid(which="major", axis="both")
+    plt.xticks(x[::50],  rotation='vertical')
+    plt.show()
 
-plt.subplot(2, 1, 2)
-plt.plot(x, plot_df.volume.values, color="green", label="Stock Volume Available")
-plt.title(f"Stock Volume of {plot_symbol} 2010-2016", fontsize=18)
-plt.xlabel("Days", fontsize=18)
-plt.ylabel("Volume", fontsize=18)
-plt.legend(loc="best")
-plt.grid(which="major", axis="both")
-plt.show()
+    plot_acf(plot_returns['close'])
+    plt.show()
+    plot_pacf(plot_returns['close'], method='ywm')
+    plt.show()
 
-# TODO: normalize first
-plot_acf(plot_df['close'].values[:100])
-plt.show()
-plot_pacf(plot_df['close'].values[:100], method='ywm')
-plt.show()
+
+if __name__ == '__main__':
+    analyze_data()
