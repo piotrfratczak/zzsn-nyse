c16528e39e87149f1fb75da723ba9687b68d7bdd
Piotr Frątczak <piotrfratczak99@gmail.com>
2022-05-31 16:29:37 +0200

    Fix GRUNet architecture

--- models/GRUNet.py
+++ models/GRUNet.py
@@ -7,19 +7,18 @@ class GRUNet(nn.Module):
     Gated Recurrent Unit.
     """
 
-    def __init__(self, input_dim, hidden_dim, output_dim, gru_layers, dropout=0.2):
+    def __init__(self, input_dim, hidden_dim, output_dim, gru_layers, pred_len, dropout=0.2):
         super(GRUNet, self).__init__()
-        self.preds_len = output_dim
+        self.pred_len = pred_len
         self.gru_layers = gru_layers
         self.hidden_dim = hidden_dim
 
-        self.gru = nn.GRU(input_dim, hidden_dim, num_layers=gru_layers, batch_first=True)
-        self.dropout = nn.Dropout(dropout)
+        self.gru = nn.GRU(input_dim, hidden_dim, num_layers=gru_layers, batch_first=True, dropout=dropout)
         self.fc = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
         h0 = torch.zeros(self.gru_layers, x.size(0), self.hidden_dim)
-        output, hn = self.gru(x, h0.detach())
-        output = self.dropout(output[:, -self.preds_len:, :])
+        output, _ = self.gru(x, h0.detach())
+        output = output[:, -self.pred_len:, :]
         output = self.fc(output)
         return output
